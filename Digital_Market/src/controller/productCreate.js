54a16369df384d41c8f59eb1ff75d9ff60c8d8b1
mherreralopardo <mherreraa97@gmail.com>
2021-12-03 10:03:04 -0300

    correcciónEnRuta

--- Digital_Market/src/controller/productCreate.js
+++ Digital_Market/src/controller/productCreate.js
@@ -1,10 +1,14 @@
 const { validationResult } = require("express-validator");
 const products = require("../database/products");
+const { appendFile } = require("fs");
+const path = require("path")
 
 const crearProducto = (req, res) => {
     const error = validationResult(req);
+    
     if (error.isEmpty()) {
            
+fs.appendFile( "/database/products.json", req, callback )
 
         res.send("Producto creado con éito");
     
