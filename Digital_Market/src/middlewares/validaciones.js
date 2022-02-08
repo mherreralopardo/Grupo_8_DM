94878335d4e7de98c3965c5026f250d60d3cb2ed
Vanina28 <vanimeni2002@gmail.com>
2022-02-08 20:16:27 -0300

    validaciones front

--- Digital_Market/src/middlewares/validaciones.js
+++ Digital_Market/src/middlewares/validaciones.js
@@ -1,7 +1,3 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 const errores = [];
-
-
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
