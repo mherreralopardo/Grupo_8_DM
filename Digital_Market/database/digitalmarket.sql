65d288fa09372cd9ea8d3a8ac0807e81b4442217
fabrivc <fabrivalcur@gmail.com>
2022-02-17 22:32:30 -0300

    Arreglando archivo .sql

--- Digital_Market/database/digitalmarket.sql
+++ Digital_Market/database/digitalmarket.sql
@@ -1,25 +1,5 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 14-01-2022 a las 04:00:43
--- Versión del servidor: 10.4.22-MariaDB
--- Versión de PHP: 8.0.13
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `digitalmarket`
---
+/*
 
 -- --------------------------------------------------------
 
@@ -124,3 +104,5 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+*/
